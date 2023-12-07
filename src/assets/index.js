6637a0652f36ba63488c30da4725541383f3b998
4th-KiNG <95131486+4th-KiNG@users.noreply.github.com>
2023-12-07 18:15:25 +0300

    main

---
+++ src/assets/index.js
@@ -0,0 +1,49 @@
+import door from './door.png'
+import phone from './phone.png'
+import clock from './clock.png'
+import star from './star.png'
+import goldDoor from './gold-door.png'
+import fon1 from './fon1.svg'
+import fon2 from './fon2.svg'
+import fon3 from './fon3.svg'
+import fon4 from './fon4.svg'
+import fon6 from './fon6.svg'
+import line from './line.png'
+import line1 from './line1.png'
+import handle1 from './handle1.png'
+import handle2 from './handle2.png'
+import handle3 from './handle3.png'
+import floorFon from './floor-fon.svg'
+import pencil from './pencil.png'
+import tool from './tool.png'
+import car from './car.png'
+import place from './place.svg'
+import email from './email.png'
+import doorIco from './door-ico.png'
+import avito from './avito.png'
+
+export{
+    door,
+    phone,
+    clock,
+    star,
+    goldDoor,
+    fon1,
+    fon2,
+    fon3,
+    fon4,
+    line,
+    handle1,
+    handle2,
+    handle3,
+    floorFon,
+    pencil,
+    car,
+    tool,
+    place,
+    email,
+    doorIco,
+    avito,
+    fon6,
+    line1
+}
