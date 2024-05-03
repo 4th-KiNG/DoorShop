6c80f03486dade11aabb3e502f492ba8fc92380b
4th-KiNG <95131486+4th-KiNG@users.noreply.github.com>
2024-05-03 20:10:02 +0300

    123123

--- src/app/catalog/furniture/page.tsx
+++ src/app/catalog/furniture/page.tsx
@@ -3,12 +3,15 @@ import '../../globals.css'
 import '../../../../public/fonts/lato.css'
 import Footer from "@/components/Footer/Footer";
 import Furniture from "@/components/Catalog/Furniture/Furniture";
+import { Suspense } from 'react'
 
 const FurniturePage = () => {
     return (
         <div>
             <Header></Header>
-            <Furniture></Furniture>
+            <Suspense>
+                <Furniture></Furniture>
+            </Suspense>
             <Footer></Footer>
         </div>
     );
