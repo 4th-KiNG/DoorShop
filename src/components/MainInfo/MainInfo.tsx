f225fdc0643e1f315044badfb18fc44d3baca6a4
4th-KiNG <95131486+4th-KiNG@users.noreply.github.com>
2024-05-07 01:07:43 +0300

    seo+

--- src/components/MainInfo/MainInfo.tsx
+++ src/components/MainInfo/MainInfo.tsx
@@ -10,7 +10,7 @@ const MainInfo = () => {
             </div>
             <div className={styles.MainText} id='maininfo'>
                 <div style={{display: "flex", flexDirection: "column", gap: "10px"}}>
-                    <h1 className={styles.MainText_Title}>Самые надёжные двери по доступным ценам</h1>
+                    <h1 className={styles.MainText_Title}>Входные и межкомнатные двери в Каснодаре по доступным ценам!</h1>
                     <div className={styles.MainText_Line}></div>
                 </div>
                 <p className={styles.MainText_Txt}>Для вас в наличии входные и межкомнатные двери, дверная фурнитура и напольное покрытие. Новинки всех производителей у нас и только для вас. Квалифицированные сотрудники, индивидуальный подход, консультация по подбору товара. Доставка в любой район города и края. Профессиональная и качественная установка дверей с гарантией от магазина. Мы ждём вас!</p>
